f070d64e699ba631e0588b03dfce2f7a031d26e5
ukokuja <ukokuja@gmail.com>
2020-05-30 19:52:36 +0300

    merge

--- includes/script.js
+++ includes/script.js
@@ -1,47 +1,72 @@
 // Article  Javascript 
+function initArticle() {
 
-$(document).ready(function () {
-    var isDisplayFooter = true;
-    $(window).on('scroll',function() {
-        var scroll = $(window).scrollTop(); // how many pixels you've scrolled
-        var descriptionTop = $('#description').offset().top; // pixels to the top of div1
-        var descriptionHeight = $('#description').height(); // height of div1 in pixels
-        var detailsTop = $('#details').offset().top; // pixels to the top of div1
-        var detailsHeight = $('#details').height(); // height of div1 in pixels
-        var itineraryTop = $('#itinerary').offset().top; // pixels to the top of div1
-        var itineraryHeight = $('#itinerary').height(); // height of div1 in pixels
-        // if you've scrolled further than the top of div1 plus it's height
-        // change the color. either by adding a class or setting a css property
-        if(scroll + 50 > descriptionTop + descriptionHeight){
-            $('.nav-item:nth-of-type(1)').removeClass('active');
-            if(scroll + 50 > detailsTop + detailsHeight){
-                $('.nav-item:nth-of-type(3)').addClass('active');
-                $('.nav-item:nth-of-type(2)').removeClass('active');
-            } else {
-                $('.nav-item:nth-of-type(2)').addClass('active');
-                $('.nav-item:nth-of-type(3)').removeClass('active');
-            }
-        } else {
-            $('.nav-item:nth-of-type(1)').addClass('active');
-            $('.nav-item:nth-of-type(2)').removeClass('active');
-        }
-        if((scroll + 300) > itineraryTop){
-            if (isDisplayFooter) {
-                $("#footer").fadeOut("fast");
-                isDisplayFooter = false;
-            }
-        } else {
-            if (!isDisplayFooter) {
-                $("#footer").fadeIn("fast");
-                isDisplayFooter = true;
-            }
-        }
+
+    $(window).on('scroll', function () {
+        var scroll = $(window).scrollTop();
+        var descriptionTop = $('#description').offset().top;
+        var descriptionHeight = $('#description').height();
+        var detailsTop = $('#details').offset().top;
+        var detailsHeight = $('#details').height();
+
+        initScrollInteraction(scroll, descriptionTop, descriptionHeight, detailsTop, detailsHeight);
     });
 
-    $("#title").change(function (e) {
-        $(".breadcrumbs h2").html(this.value);
+    initSeeMore();
+}
+function initSeeMore () {
+    $('#description a').click(function(element) {
+        $(this).parent().toggleClass('open');
     })
+}
+function initScrollInteraction(scroll, descriptionTop, descriptionHeight, detailsTop, detailsHeight) {
+    if (scroll + 50 > descriptionTop + descriptionHeight) {
+        $('.nav-item:nth-of-type(1)').removeClass('active');
+        if (scroll + 50 > detailsTop + detailsHeight) {
+            $('.nav-item:nth-of-type(3)').addClass('active');
+            $('.nav-item:nth-of-type(2)').removeClass('active');
+        } else {
+            $('.nav-item:nth-of-type(2)').addClass('active');
+            $('.nav-item:nth-of-type(3)').removeClass('active');
+        }
+    } else {
+        $('.nav-item:nth-of-type(1)').addClass('active');
+        $('.nav-item:nth-of-type(2)').removeClass('active');
+    }
+}
+function initFooterInteraction(form) {
+    $(window).on('scroll', function () {
+        var scroll = $(window).scrollTop();
+        var itineraryTop = $('#itinerary').offset().top;
+        if ((scroll + 300) > itineraryTop) {
+            $("#footer").fadeOut("fast");
+        } else {
+            $("#footer").fadeIn("fast");
+        }
+    });
+    $("#footer button").click(function (){
+        $(form).submit();
+    })
+}
+function initEditor() {
 
+    // Loop over them and prevent submission
+
+    var form = initFormValidation();
+    initBinding();
+    initModalSchedule();
+    initModalImages();
+    initFooterInteraction();
+}
+function initModalImages() {
+    $("#imagesModal figure").click(function () {
+        $("#imagesModal figure").each(function () {
+            $(this).removeClass('selected');
+        })
+        $(this).addClass('selected');
+    })
+}
+function initModalSchedule() {
     $('#editModal').on('show.bs.modal', function (event) {
         var button = $(event.relatedTarget) // Button that triggered the modal
         var timeFrom = button.data('timeFrom') // Extract info from data-* attributes
@@ -54,68 +79,117 @@ $(document).ready(function () {
         modal.find('.modal-body #place').val(place)
         modal.find('.modal-body #timeFrom').timepicker('setTime', timeFrom)
         modal.find('.modal-body #timeTo').timepicker('setTime', timeTo)
-      })
-})
-
-
-
-
-
-// Javascript on Form - Error  
-function handleData()
-{
-    var form_data = new FormData(document.querySelector("form"));
+    })
+}
+function initFormValidation() {
+    var editForm = $('#editForm');
+    editForm.submit(function (event) {
+        if (editForm[0].checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        editForm[0].classList.add('was-validated');
+    });
+    Array.prototype.filter.call(editForm, function (form) {
+        for (var i = 0; i < form.length; i++) {
+            if (form[i].className.indexOf('no-validate') < 0) {
+                form[i].oninvalid = function (e) {
+                    e.target.className = e.target.className.replace(/\bis-invalid\b/, '');
+                    e.target.className = e.target.className.replace(/\bis-valid\b/, '');
+                    e.target.className += ' is-invalid'
+                }
+                form[i].onkeydown = function (e) {
+                    e.target.className = e.target.className.replace(/\bis-invalid\b/, '');
+                    e.target.className = e.target.className.replace(/\bis-valid\b/, '');
+                }
     
-    if(!form_data.has("accessability[]")) {
-        document.getElementById("checkedError").style.visibility= "visible";
-      return false;
+                form[i].onblur = function (e) {
+                    e.target.className = e.target.className.replace(/\bis-invalid\b/, '');
+                    e.target.className = e.target.className.replace(/\bis-valid\b/, '');
+                    e.target.checkValidity();
+                    e.target.parentNode.classList.add('was-validated');
+                }
+            }
+            
+        }
+    });
+    return editForm;
+}
+function initBinding() {
+    $("#title").change(function (e) {
+        $(".breadcrumbs h2").html(this.value);
+    })
+}
+//Navigate Javascript
+function initNavigator() {
+
+    var stops = getStops();
+    var drones = getDronePositions();
+    var map = addMapWithStyle();
+    for (var i in drones) {
+        drones[i].drone = setDrone(map, drones[i].location)
+    }
+    var dots = [];
+    for (var i in stops) {
+        L.marker(stops[i].location).addTo(map);
+        dots.push(stops[i].location)
     }
 
-    else{
-        document.getElementById("checkedError").style.visibility= "hidden";
-      return true;
-    }    
+    initPath(dots, map);
+
+    getRealtimeGeoposition(map, drones)
+
+    initHamburguer();
+
+}
+function setDrone(map, pos) {
+    var myIcon = L.icon({
+        iconUrl: 'includes/images/drone.png',
+        iconSize: [50, 50],
+    });
+    return L.marker(pos, { icon: myIcon }).addTo(map);
+}
+function getStops() {
+    return [{
+        name: "Ein Gedi",
+        location: [31.469835, 35.3283313],
+    },
+    {
+        name: "First stop",
+        location: [31.465169, 35.3521382],
+    },
+    {
+        name: "Second stop",
+        location: [31.459354, 35.3579443]
+    },
+    {
+        name: "Third stop",
+        location: [31.455787, 35.3677043],
+    }]
 }
 
+function getDronePositions() {
+    return [{
+        name: "Back",
+        location: [31.465718, 35.3510653]
+    },
+    {
+        name: "Right",
+        location: [31.465718, 35.3516873]
+    },
+    {
+        name: "Front",
+        location: [31.465169, 35.3521382]
+    },
+    {
+        name: "Left",
+        location: [31.464931, 35.3515913]
+    }];
+}
 
-
-//Navigate Javascript
-
-var stops = [{
-    name: "Ein Gedi",
-    location: [31.469835,35.3283313],
-},
-{
-    name: "First stop",
-    location: [31.465169,35.3521382],
-},
-{
-    name: "Second stop",
-    location: [31.459354,35.3579443]
-},
-{
-    name: "Third stop",
-    location: [31.455787,35.3677043],
-}]
-var drones = [{
-    name: "Back",
-    location: [31.465718,35.3510653]
-},
-{
-    name: "Right",
-    location: [31.465718,35.3516873]
-},
-{
-    name: "Front",
-    location: [31.465169,35.3521382]
-},
-{
-    name: "Left",
-    location: [31.464931,35.3515913]
-}]
-$(document).ready(() => {
+function addMapWithStyle() {
     var map = L.map('map', {
-        center: [31.4664,35.387983],
+        center: [31.4664, 35.387983],
         zoom: 5,
         preferCanvas: true,
         zoomControl: false
@@ -125,45 +199,34 @@ $(document).ready(() => {
         maxZoom: 20
     });
     map.addLayer(Stadia_AlidadeSmoothDark);
+
+    map.setView(new L.LatLng(31.465718, 35.3510653), 17);
     
-    map.setView(new L.LatLng(31.465718,35.3510653), 17);
-    for (var i in drones) {
-        drones[i].drone = setDrone(map, drones[i].location)
-    }
-    var dots = [];
-    for (var i in stops) {
-        L.marker(stops[i].location).addTo(map);
-        dots.push(stops[i].location)
-    }
-    
-    var polyline = L.polyline(dots, {color: '#AAA', dashArray: "20, 15", weight: 5, 
-    lineJoin: 'round'}).addTo(map);
-
-    setInterval(function (){
-        for (var i in drones) {
-            drones[i].location = [drones[i].location[0] - 0.00005, drones[i].location[1] + 0.00005]
-            drones[i].drone.setLatLng(drones[i].location); 
-        }
-        map.panTo(new L.LatLng(drones[2].location[0], drones[2].location[1]), {animation: true});
-    }, 1000)
-
-    $('header > section').click(function (){
-        $('body').toggleClass('isHamburgerOpen');
-    })
-    $('#map').click(function(){
-        $('body').removeClass('isHamburgerOpen');
-    })
-
-})
-
-
-function setDrone (map, pos) {
-    var myIcon = L.icon({
-        iconUrl: 'includes/images/drone.png',
-        iconSize: [50, 50],
-    });
-    return L.marker(pos, {icon: myIcon}).addTo(map);
+    return map;
 }
 
+function initPath(dots, map) {
+    var polyline = L.polyline(dots, {
+        color: '#AAA', dashArray: "20, 15", weight: 5,
+        lineJoin: 'round'
+    }).addTo(map);
+}
 
+function getRealtimeGeoposition(map, drones) {
+    setInterval(function () {
+        for (var i in drones) {
+            drones[i].location = [drones[i].location[0] - 0.00005, drones[i].location[1] + 0.00005]
+            drones[i].drone.setLatLng(drones[i].location);
+        }
+        map.panTo(new L.LatLng(drones[2].location[0], drones[2].location[1]), { animation: true });
+    }, 1000)
+}
 
+function initHamburguer() {
+    $('header > section').click(function () {
+        $('body').toggleClass('isHamburgerOpen');
+    })
+    $('#map').click(function () {
+        $('body').removeClass('isHamburgerOpen');
+    })
+}
